523a03f482f9881381e1a78283879e94b82659f8
jessicamirov <jessicamirov@gmail.com>
2025-02-07 16:31:57 +0200

    task1_complete

---
+++ code/labs/terraform_exam/task1/vpc.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "az1" {
+  default = "us-east-1a"
+}
+
+variable "az2" {
+  default = "us-east-1b"
+}
+
+variable "myname" {
+  type = string
+  default = "jessica"
+}
+
+resource "aws_vpc" "jessica-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.myname}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_ip" {
+  vpc_id     = aws_vpc.aws_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = var.az1
+  tags = {
+    Name = "${var.myname}-public"
+  }
+}
+
+resource "aws_subnet" "private_ip" {
+  vpc_id     = aws_vpc.private_ip.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = var.az2
+  tags = {
+    Name = "${var.myname}-private"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.jessica-vpc.id
+  tags = {
+    Name = "Internet_getway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.jessica-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_rt.id
+  }
+  tags = {
+    Name = "public route table"
+  }
+}
+
+resource "aws_route_table_association" "public_rt" {
+  subnet_id      = aws_subnet.public_ip.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.jessica-vpc.id
+ 
+  tags = {
+    Name = "private route table"
+  }
+}
+
+resource "aws_route_table_association" "private_rt" {
+  subnet_id      = aws_subnet.private_ip.id
+  route_table_id = aws_route_table.private_rt.id
+}
