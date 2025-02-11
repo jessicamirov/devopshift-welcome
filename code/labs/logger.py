b10d88f7adaba23bdb8fb35687e09b852f0d6c09
jessicamirov <jessicamirov@gmail.com>
2025-02-11 11:55:58 +0200

    JSON OR simple logger

--- code/labs/logger.py
+++ code/labs/logger.py
@@ -1,10 +1,10 @@
+import os
+import sys
 import logging
 import json
-import os 
-import sys
 
 class JsonFormatter(logging.Formatter):
-    def format(self,record:logging.LogRecord) -> str:
+    def format(self, record: logging.LogRecord) -> str:
         log = {
             "timestamp": self.formatTime(record, self.datefmt),
             "level": record.levelname,
@@ -12,17 +12,22 @@ class JsonFormatter(logging.Formatter):
         }
         return json.dumps(log)
 
-LOG_LEVEL = os.environ.get("LOG_LEVEL", "DEBUG")
-LOG_FORMAT = os.environ.get("LOG_FORMAT", "JSON")
+def setup_logging():
+    LOG_LEVEL = os.environ.get("LOG_LEVEL", "DEBUG")
+    LOG_FORMAT = os.environ.get("LOG_FORMAT", "TEXT")
 
-logger = logging.getLogger("myapp")
-logger.setLevel(LOG_LEVEL)
-stdout_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler("log.txt")
+    logger = logging.getLogger("myapp")
+    logger.setLevel(LOG_LEVEL)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler("log.txt")
 
-if LOG_FORMAT == "JSON":
-    formatter = JsonFormatter()
-else:
-    formatter = logging.Formatter()
-
-stdout_handler.setFormatter(formatter)
+    if LOG_FORMAT == "JSON":
+        formatter = JsonFormatter()
+    else:
+        formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(module)s:%(lineno)d:%(funcName)s:%(message)s")
+        
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
