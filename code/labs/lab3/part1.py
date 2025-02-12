926ed48caa6e2be1b5335df4879f2533b272e90b
jessicamirov <jessicamirov@gmail.com>
2025-02-12 11:17:54 +0200

    fix bugs

--- code/labs/lab3/part1.py
+++ code/labs/lab3/part1.py
@@ -7,14 +7,14 @@ try:
 
     if code == 200:
         data = response.json()
-        print({"name": data.get("name")})
-        print({"email": data.get("email")})
+        print("name: ", data.get("name"))
+        print("email: ", data.get("email"))
         print(f"Address: {data['address']['street']}, {data['address']['city']}")
     elif code == 404: 
         print(f"Code {code}. User not found")
     elif code >= 500:
         print(f"Code {code}. Server error. Please try again later")
     else:
-        print ({"error": f"Unexpected error. Status code: {response.status_code}"})
-except TypeError:
+        print ("error", f"Unexpected error. Status code: {code}")
+except httpx.HTTPStatusError:
     print("I don't know what to do")
