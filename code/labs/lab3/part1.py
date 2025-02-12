f4551e8ed310eb369cffcef69dbeb55e54f7625e
jessicamirov <jessicamirov@gmail.com>
2025-02-12 11:07:37 +0200

    updates

--- code/labs/lab3/part1.py
+++ code/labs/lab3/part1.py
@@ -1,17 +1,19 @@
 import httpx
 
 try:
-    response = httpx.get('https://jsonplaceholder.typicode.com/users/1')
+    URL = "https://jsonplaceholder.typicode.com/users/1"
+    response = httpx.get(URL)
     code = response.status_code
+
     if code == 200:
         data = response.json()
         print({"name": data.get("name")})
         print({"email": data.get("email")})
         print(f"Address: {data['address']['street']}, {data['address']['city']}")
     elif code == 404: 
-        print("User not found")
-    elif code == 500: 
-        print("Server error. Please try again later")
+        print(f"Code {code}. User not found")
+    elif code >= 500:
+        print(f"Code {code}. Server error. Please try again later")
     else:
         print ({"error": f"Unexpected error. Status code: {response.status_code}"})
 except TypeError:
