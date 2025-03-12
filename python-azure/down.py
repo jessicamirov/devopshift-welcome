15c88efac0bb3afb6963c0b773defe468a273f5f
amirovjessica <amirovjessica@gmail.com>
2025-03-12 14:23:50 +0200

    down update

--- python-azure/down.py
+++ python-azure/down.py
@@ -4,6 +4,7 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.compute import ComputeManagementClient
+from azure.core.exceptions import ResourceNotFoundError
 
 subscription_id = "3f79a68d-cf0d-4291-a31f-185897f7fda1"
 resource_group_name = "jessica-resource-group"
@@ -13,7 +14,6 @@ resource_client = ResourceManagementClient(credential, subscription_id)
 network_client = NetworkManagementClient(credential, subscription_id)
 compute_client = ComputeManagementClient(credential, subscription_id)
 
-# Resource IDs from up.py (these are saved for down.py)
 resource_ids = {
     "vm_id": "jessica-vm",
     "nic_id": "jessica-nic",
@@ -22,28 +22,51 @@ resource_ids = {
     "vnet_id": "jessica-vnet"
 }
 
+
+def safe_delete(callable, *args):
+    """Helper function to safely delete a resource."""
+    try:
+        callable(*args).result()
+        print(f"Deleted {args[1]} successfully.")
+    except ResourceNotFoundError:
+        print(f"{args[1]} does not exist, skipping.")
+
+
 # Delete VM
 print(f"Deleting VM '{resource_ids['vm_id']}'...")
-compute_client.virtual_machines.begin_delete(resource_group_name, resource_ids["vm_id"]).result()
+safe_delete(compute_client.virtual_machines.begin_delete, resource_group_name, resource_ids["vm_id"])
 
 # Delete Network Interface
 print(f"Deleting Network Interface '{resource_ids['nic_id']}'...")
-network_client.network_interfaces.begin_delete(resource_group_name, resource_ids["nic_id"]).result()
+safe_delete(network_client.network_interfaces.begin_delete, resource_group_name, resource_ids["nic_id"])
 
 # Delete Public IP
 print(f"Deleting Public IP '{resource_ids['ip_id']}'...")
-network_client.public_ip_addresses.begin_delete(resource_group_name, resource_ids["ip_id"]).result()
+safe_delete(network_client.public_ip_addresses.begin_delete, resource_group_name, resource_ids["ip_id"])
 
 # Delete Subnet
-print(f"Deleting Subnet '{resource_ids['subnet_id']}'...")
-network_client.subnets.begin_delete(resource_group_name, resource_ids['vnet_id'], resource_ids["subnet_id"]).result()
+try:
+    subnet = network_client.subnets.get(resource_group_name, resource_ids['vnet_id'], resource_ids["subnet_id"])
+    print(f"Deleting Subnet '{resource_ids['subnet_id']}'...")
+    safe_delete(network_client.subnets.begin_delete, resource_group_name, resource_ids['vnet_id'], resource_ids["subnet_id"])
+except ResourceNotFoundError:
+    print(f"Subnet '{resource_ids['subnet_id']}' does not exist, skipping.")
 
 # Delete Virtual Network
-print(f"Deleting Virtual Network '{resource_ids['vnet_id']}'...")
-network_client.virtual_networks.begin_delete(resource_group_name, resource_ids['vnet_id']).result()
+try:
+    vnet = network_client.virtual_networks.get(resource_group_name, resource_ids['vnet_id'])
+    print(f"Deleting Virtual Network '{resource_ids['vnet_id']}'...")
+    safe_delete(network_client.virtual_networks.begin_delete, resource_group_name, resource_ids['vnet_id'])
+except ResourceNotFoundError:
+    print(f"Virtual Network '{resource_ids['vnet_id']}' does not exist, skipping.")
 
 # Delete Resource Group
-print(f"Deleting Resource Group '{resource_group_name}'...")
-resource_client.resource_groups.begin_delete(resource_group_name).result()
+try:
+    resource_group = resource_client.resource_groups.get(resource_group_name)
+    print(f"Deleting Resource Group '{resource_group_name}'...")
+    safe_delete(resource_client.resource_groups.begin_delete, resource_group_name)
+except ResourceNotFoundError:
+    print(f"Resource Group '{resource_group_name}' does not exist, skipping.")
 
-print("All resources deleted successfully!")
+print("All resources processed successfully!")
+
